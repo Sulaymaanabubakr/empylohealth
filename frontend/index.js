874aea5d5d68a81e896e212e0bf26c7f23228736
Sulaymaanabubakr <abubakrysulaimaan24434@gmail.com>
2026-02-16 07:25:58 +0100

    fix: remove NativeModules proxy patch causing runtime not ready crash

--- frontend/index.js
+++ frontend/index.js
@@ -5,44 +5,14 @@ console.log('[PERF] index.js: Module evaluating');
 
 require('react-native-get-random-values');
 
-const { NativeModules } = require('react-native');
 const { LogBox } = require('react-native');
 
 // Keep dev console readable while we maintain compatibility with older dev clients.
 LogBox.ignoreLogs([
-  '[expo-av]: Expo AV has been deprecated and will be removed in SDK 54.'
+  '[expo-av]: Expo AV has been deprecated and will be removed in SDK 54.',
+  '`new NativeEventEmitter()` was called with a non-null argument without the required `addListener` method.',
+  '`new NativeEventEmitter()` was called with a non-null argument without the required `removeListeners` method.'
 ]);
-
-const ensureEventEmitterCompatibility = () => {
-  const modules = NativeModules || {};
-
-  const wrapForEventEmitter = (nativeModule) => {
-    if (!nativeModule || (typeof nativeModule !== 'object' && typeof nativeModule !== 'function')) return nativeModule;
-    const hasAdd = typeof nativeModule.addListener === 'function';
-    const hasRemove = typeof nativeModule.removeListeners === 'function';
-    if (hasAdd && hasRemove) return nativeModule;
-
-    // TurboModules/HostObjects can be non-extensible; use a Proxy wrapper so NativeEventEmitter
-    // sees addListener/removeListeners without mutating the native module object.
-    return new Proxy(nativeModule, {
-      get(target, prop) {
-        if (prop === 'addListener') return () => {};
-        if (prop === 'removeListeners') return () => {};
-        return Reflect.get(target, prop);
-      }
-    });
-  };
-
-  if (modules.WebRTCModule) modules.WebRTCModule = wrapForEventEmitter(modules.WebRTCModule);
-  if (modules.DailyNativeUtils) modules.DailyNativeUtils = wrapForEventEmitter(modules.DailyNativeUtils);
-
-  // Best-effort: wrap any other modules that might be passed into NativeEventEmitter.
-  Object.keys(modules).forEach((k) => {
-    modules[k] = wrapForEventEmitter(modules[k]);
-  });
-};
-
-ensureEventEmitterCompatibility();
 
 const { registerRootComponent } = require('expo');
 const App = require('./App').default;
